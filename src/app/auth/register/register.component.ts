d71601dc11910a4868d699e30caecf9f83609b8b
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-13 19:22:16 +0100

    upload

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+public registerForm = this.fb.group({
+  name: ['Luis Carballo', [ Validators.required, Validators.minLength(3) ]],
+  email: ['test1@gamil.com', [ Validators.required, Validators.minLength(3), Validators.email ]],
+  password1: ['123456', [ Validators.required, Validators.minLength(6) ]],
+  password2: ['123456', [ Validators.required, Validators.minLength(6) ]],
+  term: [false, [ Validators.required ]],
+});
 
+constructor(
+
+  private fb: FormBuilder
+
+) {}
+
+createUser() {
+
+  console.log(this.registerForm.value);
+
+}
 
 }
