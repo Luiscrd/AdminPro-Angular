79979b1ba4c0868a525238a936cf6f68c5608797
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-14 15:13:35 +0100

    upload

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  base_url: 'http://localhost:3000/api/v1',
-  GOOGLE_ID: '357726596686-ohmmcs7j932k4entgi329evnb2p5f352.apps.googleusercontent.com',
-  GOOGLE_SECRET: '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',
-  urlBackEnd: 'http://localhost:3000/api/v1/login/google'
-
-};
